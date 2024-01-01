7d7e71ac636a8714753376ff43534deccff1679a
laksamanaap <laksamana.arya1412@gmail.com>
2024-01-01 16:41:34 +0700

    feat : init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 600;
+
+ctx.fillStyle = "#414141";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
